106eeb5a8c61eb13c62679fe94a0c33108efbc07
Alex Mathews <alexjmathews@gmail.com>
2016-04-20 21:51:38 -0400

    twilio confirmiation

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ angularApp.controller('testController', function($scope) {
 
 });
 
-angularApp.controller('signupController', function($scope) {
+angularApp.controller('signupController', function($scope, $http) {
 // create a message to display in our view
   $scope.user = {
     "name":"",
@@ -21,11 +21,37 @@ angularApp.controller('signupController', function($scope) {
     "password":"",
     "repPassword":"",
     "phone": "",
-    "orgName": ""
+    "orgName": "",
+    "csc":"",
+    "zip":""
 
 
   };
   $scope.submit = function() {
     console.log($scope.user);
+  }
+
+  $scope.test = function() {
+    console.log("Phone number changed");
+    $http({
+      method: 'POST',
+      url: 'https://api.fusiform.co/tvaccess/register/verify/phone',
+      data: {
+        phone:$scope.user.phone
+      }
+    }).then(function successCallback(response) {
+        // this callback will be called asynchronously
+        // when the response is available
+        console.log(response)
+        if (response.data.success) {
+          $scope.user.phone = response.data.formatted;
+        } else {
+          console.log("invalid")
+        }
+      }, function errorCallback(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+          console.log("invalid")
+      });
   }
 });
